0081c5a091dd219b7d574c8bee0cd58e9eb2247f
Kevin <64626661+kmc6123@users.noreply.github.com>
2021-05-18 11:46:48 -0400

    Update Web_Scraper_BestBuy.py

--- Web_Scraper_BestBuy.py
+++ Web_Scraper_BestBuy.py
@@ -1,17 +1,38 @@
-from requests_html import HTMLSession
-import pandas as pd
+try:
+    from requests_html import HTMLSession   
+except ModuleNotFoundError:  
+    print("Module 'requests-html' is not installed and is required for use of this program")
+    print("\nQuitting program")
+    quit()
+
+try:
+    import pandas as pd
+except ModuleNotFoundError:
+    print("Module 'pandas' is not installed and is required for use of this program")
+    print("\nQuitting program")
+    quit()
+
+try:
+    import sqlalchemy as sq
+    installed = True
+except ModuleNotFoundError:
+    print("Module 'sqlalchemy' is not installed and may impact the functionality of this program")
+    installed = False
+
+
 import re
 
 from time import sleep
 from random import randint
 
 import getpass
+import platform
 import os
-import sqlalchemy as sq
 
 
 def inputPageNum(max_pagenum):
-    response = int(input("Enter number of pages to scrape (default is 1): ") or "1")
+    response = int(
+        input("Enter number of pages to scrape (default is 1): ") or "1")
 
     while (response > max_pagenum):
         print("\nERROR: Number of pages entered exceeds the number of pages available " + "(" + str(max_pagenum) + ")")
@@ -26,7 +47,8 @@ def scrapeData(webpages, session, baseUrl):
         items = r.html.find("#main-results > ol > li.sku-item")
 
         for item in items:
-            item_price = item.find("div.priceView-hero-price.priceView-customer-price", first=True)
+            item_price = item.find(
+                "div.priceView-hero-price.priceView-customer-price", first=True)
             if item_price != None:
                 clean_string = re.sub("\$|\,|(Y.+)", '', item_price.text)
                 price.append(float(clean_string))
@@ -47,14 +69,22 @@ def scrapeData(webpages, session, baseUrl):
 
         sleep(randint(2, 6))
 
-        
+
 def toCSV(df):
     username = getpass.getuser()
-    input_filename = input("\nEnter name for the file (no extention required): ")
+    input_filename = input(
+        "\nEnter name for the file (no extention required): ") or "BestBuy_Web_Scrape"
 
-    df.to_csv("/Users/" + username + "/Downloads/" + input_filename + ".csv", index=False, encoding='utf-8-sig')
+    plt = platform.system()
+    if (plt == "Windows"):
+        df.to_csv("C:/Users/" + username + "/Downloads/" +
+                  input_filename + ".csv", index=False, encoding='utf-8-sig')
 
-    
+    if (plt == "Darwin"):
+        df.to_csv("/Users/" + username + "/Downloads/" +
+                  input_filename + ".csv", index=False, encoding='utf-8-sig')
+
+
 def toMySQL(df):
     mysql_username = os.environ.get("DB_USER")
     mysql_password = os.environ.get("DB_PASS")
@@ -64,7 +94,7 @@ def toMySQL(df):
 
     df.to_sql("BB_Table", con, index=False, if_exists="replace")
 
-    
+
 def selectExportOption(df):
     optionMenu = "\nExport Options:\n1.) To CSV File\n2.) To MySQL Database\n\nPlease type option number and hit enter to select: "
     finished = False
@@ -76,15 +106,18 @@ def selectExportOption(df):
             print("DONE: CSV file is in your downloads folder")
             break
 
-        elif selection == "2":
+        elif selection == "2" and installed:
             toMySQL(df)
             print("DONE: Data was sent to MySQL database and stored")
             break
 
+        elif selection == "2" and not installed:
+            print("ERROR: Module 'sqlalchemy' is not installed only option CSV is available")
+
         elif selection != ("1" or "2"):
             print("ERROR: Invalid selection please type either 1 or 2 to complete")
 
-            
+
 def main():
     print("*************************BESTBUY_WEB_SCRAPER*************************\n")
 
@@ -93,7 +126,7 @@ def main():
     baseUrl = "https://www.bestbuy.com/site/refrigerators/french-door-refrigerators/abcat0901004.c"
 
     max_pagenum = int(session.get(baseUrl).html.find("ol.paging-list > li:nth-last-child(1)", first=True).text)
-    
+
     input_pagenum = inputPageNum(max_pagenum)
     
     webpages = range(1, input_pagenum + 1)
@@ -106,7 +139,7 @@ def main():
 
     selectExportOption(df)
 
-    
+
 price = []
 brand = []
 model = []
